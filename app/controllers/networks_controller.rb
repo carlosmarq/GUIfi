fb5d27320151d4024cfc546276ee203ff5a121dc
carlos <carlosmarq@gmail.com>
2017-09-27 21:55:43 -0500

    add cvs loader

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -1,4 +1,5 @@
 class NetworksController < ApplicationController
+      require 'csv'
 
 
   def test
@@ -6,14 +7,14 @@ class NetworksController < ApplicationController
   end
 
   def load
-    require 'csv'
 
-csv_text = File.read('seed.csv-01.csv')
-csv = CSV.parse(csv_text, :headers => true)
-csv.each do |row|
-Moulding.create!(row.to_hash)
-end
+    Networks_text = CSV.read("#{Rails.root}/public/seed0.csv-01.csv")
+
+    CSV.foreach("#{Rails.root}/public/seed0.csv-01.csv") do |row|
+
+    Network.create({bssid: row[0], First_time_seen: row[1], Last_time_seen: row[2],channel: row[3], Speed: row[4],Privacy: row[5], Cipher: row[6], Authentication: row[7], Power: row[8], beacons: row[9], IV: row[10], LAN_IP: row[11], ID_lenght: row[12], Key: row[13], ESSID: row[14],})
   end
+
 
   def update
 
