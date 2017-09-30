719735372e4ea805733a6120d3d32a0a21d92152
Carlos <carlosmarq@gmailcom>
2017-09-29 21:57:41 -0500

    ading comments and airmon commands

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -8,18 +8,18 @@ class NetworksController < ApplicationController
     @Hostname = `hostname`
     @User = `whoami`
     @NetworkInf = `ifconfig`
+    @airmonstat = `airmon-ng`
+    @airmoncheck = `airmon-ng check`
+    @airmonkill = `airmon-ng check kill`
+    @airmonstart = `airmon-ng start wlan0`
   end
 
   def test
-
-    @out = %x[ ls ]
-
+    @airmonstart = `airmon-ng start wlan0`
   end
 
   def load
-
     #Networks_text = CSV.read("#{Rails.root}/public/seed0.csv-01.csv")
-
       CSV.foreach("#{Rails.root}/public/seed0.csv-01.csv") do |row|
       Network.create({bssid: row[0], First_time_seen: row[1], Last_time_seen: row[2],channel: row[3], Speed: row[4],Privacy: row[5], Cipher: row[6], Authentication: row[7], Power: row[8], beacons: row[9], IV: row[10], LAN_IP: row[11], ID_lenght: row[12], Key: row[13], ESSID: row[14],})
       end
@@ -35,6 +35,11 @@ class NetworksController < ApplicationController
   end
 
   def attack
+    @ticket = Network.find(params[:id])
+    @Network_target_bssid = Network.bssid
+
+    `pdftk #{filename} burst output #{filename_out}`
+
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get '/networks/test' => 'networks#test', as: :test
   get '/networks/load' => 'networks#load', as: :load
+  get '/networks/:id/attack' => 'networks#attack', as: :attack
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
